f03f16bf97e6a6a83dfca89ff034cb45bc2b7d73
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-08-17 15:55:03 +0200

    updated script to show dates as well

--- repo_count_history.sh
+++ repo_count_history.sh
@@ -2,4 +2,5 @@
 
 # Thanks @ddunbar!
 
-for i in $(git log --format=oneline | cut -d\  -f1); do git show ${i}:README.md | grep analyzed | cut -d\  -f7; done
+# for i in $(git log --format=oneline | cut -d\  -f1); do git show ${i}:README.md | grep analyzed | cut -d\  -f7; done
+for i in $(git log --format=oneline | cut -d\  -f1); do git show ${i}:README.md | grep analyzed; done
